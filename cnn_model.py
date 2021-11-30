f63c60d3c5f4194122893137b4ddb7f000dc31ea
piotr_cenda <piotr.cenda222@gmail.com>
2021-12-01 00:23:42 +0100

    antoher model weights update

--- cnn_model.py
+++ cnn_model.py
@@ -9,41 +9,30 @@ class mask_net(nn.Module):
         self.conv1 = nn.Sequential(nn.Conv2d(in_channels, 16, kernel_size=3, padding=1),
                                   nn.BatchNorm2d(16),
                                   nn.ReLU(inplace=True),
-                                  nn.MaxPool2d(4)) # out: 16 x 64 x 64
+                                  nn.MaxPool2d(8)) # out: 16 x 32 x 32
+        
+        self.res1 = nn.Sequential(nn.Conv2d(16, 64, kernel_size=3, padding=1),
+                                  nn.BatchNorm2d(64),
+                                  nn.ReLU(inplace=True)) # out: 64 x 32 x 32
 
-        self.conv2 = nn.Sequential(nn.Conv2d(16, 128, kernel_size=3, padding=1),
+        self.conv2 = nn.Sequential(nn.Conv2d(64, 128, kernel_size=3, padding=1),
                                   nn.BatchNorm2d(128),
                                   nn.ReLU(inplace=True),
-                                  nn.MaxPool2d(4)) # out: 128 x 16 x 16
-
-        self.res1 = nn.Sequential(nn.Conv2d(128, 128, kernel_size=3, padding=1),
-                                  nn.BatchNorm2d(128),
-                                  nn.ReLU(inplace=True)) # out: 128 x 16 x 16
-
+                                  nn.MaxPool2d(8)) # out: 128 x 4 x 4
+        
         self.conv3 = nn.Sequential(nn.Conv2d(128, 256, kernel_size=3, padding=1),
                                   nn.BatchNorm2d(256),
                                   nn.ReLU(inplace=True),
-                                  nn.MaxPool2d(4)) # out: 256 x 4 x 4
-        
-        self.res2 = nn.Sequential(nn.Conv2d(256, 256, kernel_size=3, padding=1),
-                                  nn.BatchNorm2d(256),
-                                  nn.ReLU(inplace=True)) # out: 256 x 4 x 4
-        
-        self.conv4 = nn.Sequential(nn.Conv2d(256, 512, kernel_size=3, padding=1),
-                                  nn.BatchNorm2d(512),
-                                  nn.ReLU(inplace=True),
-                                  nn.MaxPool2d(4)) # out: 512 x 1 x 1
+                                  nn.MaxPool2d(8)) # out: 256 x 1 x 
 
-        self.classifier = nn.Sequential(nn.Flatten(),  # out: 512
-                                        nn.Linear(512, 128),
-                                        nn.Linear(128, num_classes))  # out: 2
+        self.classifier = nn.Sequential(nn.Flatten(),  # out: 256
+                                        nn.Linear(256, 512),
+                                        nn.Linear(512, num_classes))  # out: 2
 
     def forward(self, inpt):
         out = self.conv1(inpt)
-        out = self.conv2(out)
         out = self.res1(out)
+        out = self.conv2(out)
         out = self.conv3(out)
-        out = self.res2(out)
-        out = self.conv4(out)
         out = self.classifier(out)
         return out
